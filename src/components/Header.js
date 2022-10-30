1c9ccd735b328e14eb230644f680836b15e068f2
Marcos Aguero <aguermar@gmail.com>
2022-10-30 12:29:04 -0300

    cv

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
             <img src={hand} className="hand" alt="hand"/>
             <br/> Mi nombre es <b>Marcos Agüero</b>.
             </h1>
-            <p style={{color:"white"}}>Frontend developer | Ilustrador | Estudiante avanzado de ingeniería informática</p>
+            <p style={{color:"white"}}>Frontend developer | Ilustrador</p>
             <p style={{color:"#ababab"}}>Con el objetivo de capacitarme al máximo sobre el front end y desarrollo de videojuegos, áreas que realmente 
                 me apasionan y donde puedo integrar mis conocimientos tanto de arte como de programación.
             </p>
